a68e608fb7b96441748027601df7dabbf397c7a4
Martin Olanders <34156249+olanders-se@users.noreply.github.com>
2025-08-14 09:12:27 +0000

    Chapter 2

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,29 @@
 
 #include <iostream>
 
+int a, b = 5;  // B tilldelas 5
+
 int main(){
+    bool myFlag;
+
+    a = 7;
+    myFlag = false;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "myFlag = " << myFlag << std::endl;
+    std::cout << std::endl << std::endl;
     
+    myFlag = true;
+
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    std::cout << "myFlag = " << myFlag << std::endl;
+
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b -a (unsigned) = " << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
