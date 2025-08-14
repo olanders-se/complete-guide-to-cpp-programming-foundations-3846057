c31875582b1f36194cdc117c4fab887a7bcbe2a5
Martin Olanders <34156249+olanders-se@users.noreply.github.com>
2025-08-14 11:50:04 +0000

    Fler uppgifter

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -8,6 +8,18 @@
 int main(){
     float target_x;
     int32_t sprite_x;
+    uint32_t player_x;
+
+    target_x = -123.45;
+    sprite_x = target_x;
+    player_x = sprite_x;
+
+    std::cout << "target_x float: " << target_x << std::endl;
+    std::cout << "sprite_x int32: " << sprite_x << std::endl;
+    std::cout << "player_x unsigned 32: " << player_x << std::endl;
+    std::cout << "player_x int32: " << static_cast<int32_t>(player_x) << std::endl;
+    std::cout << "player_x float: " << static_cast<float>(player_x) << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
