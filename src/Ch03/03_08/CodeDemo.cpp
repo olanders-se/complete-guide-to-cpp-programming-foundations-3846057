0acbd37b84d437cb2a2d67e5325275dcb3cae2dc
Martin Olanders <34156249+olanders-se@users.noreply.github.com>
2025-08-15 09:46:36 +0000

    Code Challenge

--- src/Ch03/03_08/CodeDemo.cpp
+++ src/Ch03/03_08/CodeDemo.cpp
@@ -8,15 +8,15 @@
 double my_average(int a, int8_t b, uint32_t c){
     double result = 0.0;
 
-    // Write your code here
-    
+    //result = ((double)a + (double)b + (double)c) / 3; 
+    result = (a + b + c) / 3.0;
     return result;
 }
 
 int main(){
-    int a = 10;
-    int8_t b = 21;
-    uint32_t c = 30;
+    int a = -12;
+    int8_t b = 15;
+    uint32_t c = 10;
     double learnerResult = my_average(a, b, c);
     
     std::cout << "Your code returned: " << learnerResult << std::endl;
