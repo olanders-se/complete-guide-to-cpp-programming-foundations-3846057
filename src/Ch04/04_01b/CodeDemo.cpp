55c912b748df7b8117b4198055ce874369029258
Martin Olanders <34156249+olanders-se@users.noreply.github.com>
2025-08-18 09:01:25 +0000

    Början kapitel 4

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -4,8 +4,32 @@
 
 #include <iostream>
 
+//#define Level_Count 4  // Without semicolon
+
 int main(){
-    
+    const size_t Level_Count = 4;
+    int enemies[Level_Count];
+    float levelDifficulty[] = {1.0, 3.5, 6.0, 10.5};
+
+    enemies[0] = 10;
+    enemies[1] = 15;
+    enemies[2] = 20;
+    enemies[3] = 25;
+
+    std::cout << "The game has 4 levels" << std::endl;
+    std::cout << "Value in 1 level 1: " << enemies[0] << " enemies." << std::endl;
+    std::cout << "Value in 2 level 2: " << enemies[1] << " enemies." << std::endl;
+    std::cout << "Value in 3 level 3: " << enemies[2] << " enemies." << std::endl;
+    std::cout << "Value in 4 level 4: " << enemies[3] << " enemies." << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << "Level difficulties are set as follows:" << std::endl;
+    std::cout << "Level 1: " << levelDifficulty[0] <<  std::endl;
+    std::cout << "Level 2: " << levelDifficulty[1] <<  std::endl;
+    std::cout << "Level 3: " << levelDifficulty[2] <<  std::endl;
+    std::cout << "Level 4: " << levelDifficulty[3] <<  std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
