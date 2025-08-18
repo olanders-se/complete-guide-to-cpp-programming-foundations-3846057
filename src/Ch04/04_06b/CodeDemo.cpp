3f904aa5a84623264f53d002f707ac26b86f0861
Martin Olanders <34156249+olanders-se@users.noreply.github.com>
2025-08-18 12:12:12 +0000

    Kapitel 4

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -2,11 +2,25 @@
 // Exercise 04_06
 // The Vector Class, by Eduardo Corpeño 
 
+#include <vector>
 #include <iostream>
 #include <string>
 
 int main(){
-    
+    std::vector<std::string> checkpoints = {"Start", "Forest", "Castle"};
+
+    checkpoints.push_back("Cave");
+    checkpoints.push_back("Finish");
+
+    std::cout << "The game has " << checkpoints.size() << " Checkpoints" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "3e element is " << checkpoints[2] <<  std::endl;
+
+    checkpoints[2] = "Dark Castle";
+    std::cout << std::endl;
+    std::cout << "3e element is now " << checkpoints[2] <<  std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
