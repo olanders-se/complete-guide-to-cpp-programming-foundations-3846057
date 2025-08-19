4f9c32f8ac7fed4166b9bb43fe39c377536380e7
Martin Olanders <34156249+olanders-se@users.noreply.github.com>
2025-08-19 07:23:06 +0000

    Pågående på 5

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -8,6 +8,16 @@ int main(){
     int score = 1023;
     bool gamePaused = false;
     char action = 'x';
+
+    if (score > 1000)
+        std::cout << "Your score is over 1000." << sd:endl;
+    if (score % 2)
+        std::cout << "Score is odd" << std::endl;
+    else
+        std::cout << "Score is even" << std:: endl;
+
+        std::cout 
+    
     
     std::cout << std::endl << std::endl;
     return 0;
