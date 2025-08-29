21fde9f101bb019a964b12017413764c37c83732
Martin Olanders <34156249+olanders-se@users.noreply.github.com>
2025-08-29 11:34:03 +0000

    end kap 7

--- src/Ch07/07_08b/inventory.cpp
+++ src/Ch07/07_08b/inventory.cpp
@@ -15,25 +15,27 @@ Inventory::~Inventory(){
     delete items; // Prevent memory leak by deallocating the dynamic vector
 }
 
-// Add item to inventory
-void Inventory::addItem(const std::string& item){
+// Add item to inventory Overload += operator to add an item
+Inventory& Inventory::operator+=(const std::string& item){
     if (items->size() < capacity)
         items->push_back(item);
     else
         std::cout << "Inventory is full, cannot add " << item << std::endl;
+    return *this;
 }
 
-// Remove item from inventory
-void Inventory::removeItem(const std::string& item){
+// Remove item from inventory Overload -= operator to remove an item
+Inventory& Inventory::operator-=(const std::string& item){
     auto it = std::find(items->begin(), items->end(), item);
     if (it != items->end())
         items->erase(it);
     else
         std::cout << "Item " << item << " not found in inventory" << std::endl;
+    return *this;
 }
 
-// Access item by index
-std::string Inventory::getItem(int index) const{
+// Access item by index  Overload [] operator to acces item by index
+std::string Inventory::operator[](int index) const{
     if (index >= 0 && index < items->size())
         return (*items)[index];
     else
